6088183efe93a4a0ce8a8acec79790afe6313b80
Paul Harrington <pharring@microsoft.com>
2017-11-15 15:40:45 -0800

    Update comments on Abramowitz and Stegun algorithm

--- src/Gaussian.cs
+++ src/Gaussian.cs
@@ -16,39 +16,52 @@ namespace Timewaster
         public double NextGaussian(double dMu, double dSigma) =>
             dMu + CumulativeGaussian(_rnd.NextDouble()) * dSigma;
 
-        private static double CumulativeGaussian(double p)
+        /// <summary>
+        /// Apply the inverse cumulative Gaussian distribution function.
+        /// i.e. convert a rectangular probability into a Gaussian distribution.
+        /// </summary>
+        /// <param name="p">
+        /// A value between 0 and 1 representing a probability from a rectangular
+        /// distribution.
+        /// </param>
+        /// <returns>
+        /// A value x where Q(x)=psuch that <code>Prob(Z &lt; x) = p</code> where Z is the
+        /// standard normal (Gaussian) distribution.
+        /// </returns>
+        /// <remarks>
+        /// Because of the symmetry of the normal distribution, we need
+        /// only consider 0 &lt; p &lt;= 0.5. For p &gt; 0.5, apply the algorithm
+        /// to 1-p and negate the result.
+        /// </remarks>
+        private static double CumulativeGaussian(double p) => 
+            p <= 0.5 ? Approximation(p) : -Approximation(1 - p);
+
+        /// <summary>
+        /// This is an approximation by Abramowitz and Stegun.
+        /// See 26.2.23 at http://people.math.sfu.ca/~cbm/aands/page_933.htm
+        /// </summary>
+        /// <param name="p">
+        /// Input probability (rectangular). Must be between 0 and 0.5.
+        /// </param>
+        /// <returns>
+        /// A rational approximation for x where Q(x)=p and Q is the
+        /// complementary cumulative distribution function for a standard
+        /// normal distribution.
+        /// </returns>
+        private static double Approximation(double p)
         {
-            // p is a rectangular probability between 0 and 1
-            // convert that into a gaussian.
-            // Apply the inverse cumulative gaussian distribution function
-            // This is an approximation by Abramowitz and Stegun; Press, et al.
-            // See http://www.pitt.edu/~wpilib/statfaq/gaussfaq.html
-            // Because of the symmetry of the normal
-            // distribution, we need only consider 0 < p < 0.5. If you have p > 0.5,
-            // then apply the algorithm below to q = 1-p, and then negate the value
-            // for X obtained.
-            bool fNegate = false;
-
-            if (p > 0.5)
-            {
-                p = 1.0 - p;
-                fNegate = true;
-            }
-
             //double t = Math.Sqrt(Math.Log(1.0 / (p * p)));
             double t = Math.Sqrt(-2.0 * Math.Log(p));
             double tt = t * t;
             double ttt = tt * t;
-            double X = t - ((c_0 + c_1 * t + c_2 * tt) / (1 + d_1 * t + d_2 * tt + d_3 * ttt));
-
-            return fNegate ? -X : X;
+            return t - ((c0 + c1 * t + c2 * tt) / (1 + d1 * t + d2 * tt + d3 * ttt));
         }
 
-        private const double c_0 = 2.515517;
-        private const double c_1 = 0.802853;
-        private const double c_2 = 0.010328;
-        private const double d_1 = 1.432788;
-        private const double d_2 = 0.189269;
-        private const double d_3 = 0.001308;
+        private const double c0 = 2.515517;
+        private const double c1 = 0.802853;
+        private const double c2 = 0.010328;
+        private const double d1 = 1.432788;
+        private const double d2 = 0.189269;
+        private const double d3 = 0.001308;
     }
 }
