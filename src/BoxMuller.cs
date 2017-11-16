fe3518fc199a56337effc6a4e9f6f6d09c27afa6
Paul Harrington <pharring@microsoft.com>
2017-11-15 17:05:34 -0800

    Introduce Tau constant

--- src/BoxMuller.cs
+++ src/BoxMuller.cs
@@ -5,6 +5,7 @@ namespace Timewaster
     internal class BoxMullerRandom : IGaussianRandom
     {
         private readonly Random _rnd = new Random();
+        private const double Tau = Math.PI * 2;
 
         /// <summary>
         /// Choose the next random number. The distribution of randomness is "normal"
@@ -20,7 +21,7 @@ namespace Timewaster
         {
             double u1 = 1.0 - _rnd.NextDouble();
             double u2 = 1.0 - _rnd.NextDouble();
-            return Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+            return Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(Tau * u2);
         }
     }
 }
